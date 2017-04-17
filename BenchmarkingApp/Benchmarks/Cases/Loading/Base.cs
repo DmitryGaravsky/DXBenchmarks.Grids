fad82689a5c451a21945493ec5cea6d873ca77ed
DmitryGaravsky <garavskydmitry@gmail.com>
2017-04-17 13:40:22 +0300

    Loading benchmarks
    Loading benchmarks added

---
+++ BenchmarkingApp/Benchmarks/Cases/Loading/Base.cs
@@ -0,0 +1,20 @@
+﻿namespace BenchmarkingApp.Tree {
+    using System.Collections.Generic;
+    using BenchmarkingApp.Benchmarks.Data;
+    using DevExpress.XtraTreeList;
+
+    [BenchmarkHost("TreeList")]
+    public abstract class LoadBase : IBenchmarkItem {
+        protected TreeList treeList;
+        public virtual void SetUp(object uiControl) {
+            treeList = ((TreeList)uiControl);
+            treeList.DataSource = null;
+            treeList.Columns.Clear();
+            treeList.Nodes.Clear();
+            treeList.OptionsBehavior.PopulateServiceColumns = true;
+        }
+        public virtual void TearDown(object uiControl) {
+        }
+        public abstract void Benchmark();
+    }
+}
---
+++ BenchmarkingApp/Benchmarks/Cases/Loading/Loading.cs
@@ -0,0 +1,85 @@
+﻿namespace BenchmarkingApp.Tree.Bound {
+    using System.Collections.Generic;
+    using BenchmarkingApp.Benchmarks.Data;
+
+    [BenchmarkItem("Loading (Bound)", Configuration = "Huge")]
+    public class Loading : LoadBase {
+        List<Row> dataSource;
+        public sealed override void SetUp(object uiControl) {
+            Row.EnsureListSource(ref dataSource, Configuration.Current.Rows);
+            base.SetUp(uiControl);
+        }
+        public sealed override void Benchmark() {
+            treeList.DataSource = dataSource;
+        }
+    }
+}
+namespace BenchmarkingApp.Tree.BoundHierarchy {
+    using System.Collections.Generic;
+    using BenchmarkingApp.Benchmarks.Data;
+
+    [BenchmarkItem("Loading (Bound Hierarchy)", Configuration = "Huge")]
+    public class Loading : LoadBase {
+        List<HierarchicalRow> dataSource;
+        public sealed override void SetUp(object uiControl) {
+            Row.EnsureHierarchicalSource(ref dataSource, Configuration.Current.Rows, Configuration.Current.Levels);
+            base.SetUp(uiControl);
+        }
+        public sealed override void Benchmark() {
+            treeList.DataSource = dataSource;
+        }
+    }
+}
+namespace BenchmarkingApp.Tree.Unbound {
+    using System.Collections.Generic;
+    using BenchmarkingApp.Benchmarks.Data;
+
+    [BenchmarkItem("Loading (Unbound)", Configuration = "Huge")]
+    public class Loading : LoadBase {
+        List<Row> dataSource;
+        public sealed override void SetUp(object uiControl) {
+            Row.EnsureListSource(ref dataSource, Configuration.Current.Rows);
+            base.SetUp(uiControl);
+            // Columns
+            treeList.BeginUpdate();
+            var columns = Row.GetColumns();
+            for(int i = 0; i < columns.Length; i++)
+                treeList.Columns.AddVisible(columns[i]);
+            treeList.EndUpdate();
+        }
+        public sealed override void Benchmark() {
+            treeList.BeginUnboundLoad();
+            for(int i = 0; i < dataSource.Count; i++)
+                treeList.Nodes.Add(dataSource[i].GetData());
+            treeList.EndUnboundLoad();
+        }
+    }
+}
+namespace BenchmarkingApp.Tree.UnboundHierarchy {
+    using System.Collections.Generic;
+    using BenchmarkingApp.Benchmarks.Data;
+
+    [BenchmarkItem("Loading (Unbound Hierarchy)", Configuration = "Huge")]
+    public class Loading : LoadBase {
+        List<HierarchicalRow> dataSource;
+        public sealed override void SetUp(object uiControl) {
+            Row.EnsureHierarchicalSource(ref dataSource, Configuration.Current.Rows, Configuration.Current.Levels);
+            base.SetUp(uiControl);
+            // Columns
+            treeList.BeginUpdate();
+            var columns = HierarchicalRow.GetHierarchicalColumns();
+            for(int i = 0; i < columns.Length; i++)
+                treeList.Columns.AddVisible(columns[i]);
+            treeList.EndUpdate();
+        }
+        public sealed override void Benchmark() {
+            treeList.BeginUnboundLoad();
+            for(int i = 0; i < dataSource.Count; i++) {
+                var data = dataSource[i].GetHierarchicalData();
+                treeList.AppendNode(data, dataSource[i].ParentID);
+            }
+            treeList.ExpandAll();
+            treeList.EndUnboundLoad();
+        }
+    }
+}
