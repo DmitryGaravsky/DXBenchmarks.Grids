dfb104ccdaf9950fb1ad9bf077036bb9e6cdd12d
DmitryGaravsky <garavskydmitry@gmail.com>
2017-04-20 13:37:55 +0300

    fix
    fix

--- BenchmarkingApp/RunHuge.cmd
+++ BenchmarkingApp/RunHuge.cmd
@@ -3,8 +3,5 @@ ngen install BenchmarkingApp.exe /nologo /silent
 
 BenchmarkingApp Workloads/Sorting.Huge.workload
 BenchmarkingApp Workloads/Filtering.Huge.workload
-BenchmarkingApp Workloads/Loading.Huge.workload
 BenchmarkingApp Workloads/Searching.Huge.workload
-
-BenchmarkingApp Workloads/Sorting.Deep.workload
-BenchmarkingApp Workloads/Filtering.Deep.workload
+BenchmarkingApp Workloads/Loading.Huge.workload
